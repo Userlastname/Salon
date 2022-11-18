dc34ee16d597c62df3023900194cce922e9665ca
Userlastname <zhmerinka1pasha@gmail.com>
2022-11-18 15:27:00 +0200

    Added locales and new title and description for Welcome page

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module TattoSalon
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.I18n.default_locale = :uk
   end
 end
